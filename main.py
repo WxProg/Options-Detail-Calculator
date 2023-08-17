79db68bc6434e5472d0ec484b14fca6198bdb0ab
mysta <chadmysta18@gmail.com>
2023-08-17 01:57:01 -0500

    Fixed PyCharm Syntax Errors

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ FRAME_BG = '#2E2E2E'
 EXIT_BUTTON_COLOR = '#D32F2F'
 EXIT_BUTTON_HOVER_COLOR = '#C62828'
 
+
 # Function to terminate the GUI
 def close_app():
     window.destroy()
+
 
 def reset_fields():
     """Reset all entry fields and result variables."""
@@ -36,16 +38,20 @@ def reset_fields():
     net_profit_var.set("")
 
 
-def create_label(window, text, row):
-    label = ttk.Label(window, text=text, font=PRIMARY_FONT)
-    label.grid(row=row, column=0, sticky=W+E, padx=8, pady=6)
+def create_label(window_to_use, text, row):
+    label = ttk.Label(window_to_use, text=text, font=PRIMARY_FONT)
+    label.grid(row=row, column=0, sticky=W + E, padx=8, pady=6)
 
     return label
-def create_entry(window, var, row):
-    entry = ttk.Entry(window, width=ENTRY_WIDTH, textvariable=var)
+
+
+def create_entry(window_to_use, var, row):
+    entry = ttk.Entry(window_to_use, width=ENTRY_WIDTH, textvariable=var)
     entry.grid(row=row, column=1, sticky=E)
 
     return entry
+
+
 def calculate_option_details():
     try:
         # Get the values from GUI entry fields and convert them to floats
@@ -53,7 +59,7 @@ def calculate_option_details():
         target_price = float(target_price_var.get())
         strike_price = float(strike_price_var.get())
         premium_per_share = float(premium_per_share_var.get())
-        premium_paid = premium_per_share * 100 # Convert per-share premium to total premium for 100 shares
+        premium_paid = premium_per_share * 100  # Convert per-share premium to total premium for 100 shares
 
         # Determine the intrinsic value and current option status
         tolerance = 0.05  # Define tolerance level for at-the-money options
@@ -68,7 +74,6 @@ def calculate_option_details():
             option_status = "Out-of-the-Money (OTM)"
             intrinsic_value = 0
 
-
         if premium_per_share == 0:
             messagebox.showwarning(title="Input Warning", message="Premium paid, cannot be zero.")
             return
@@ -76,14 +81,14 @@ def calculate_option_details():
         # Intrinsic value is the amount by which an option is in-the-money.
         #  For a call option, it is cal. as the difference between the stock price and the strike price
         #  only when the stock price is above the strike price
-        future_intrinsic_value = max(target_price - strike_price, 0)  *100
+        future_intrinsic_value = max(target_price - strike_price, 0) * 100
 
         # Compute future profitability of the option & net profit.
         profit = (future_intrinsic_value - premium_paid)
         net_profit = profit - premium_paid
 
-        #Determine the percentage return
-        percentage_return = (profit/premium_paid) * 100
+        # Determine the percentage return
+        percentage_return = (profit / premium_paid) * 100
 
         # Display results on GUI
         option_status_var.set(f"Option Status: {option_status}")
@@ -97,17 +102,16 @@ def calculate_option_details():
         return
 
 
-
 # Setup main window
 window = Tk()
 window.title("Options Details Calculator")
 window.minsize(width=400, height=400)
 window.resizable(False, False)
-window.config(bg=BACKGROUND_COLOR,padx=10, pady=10)
+window.config(bg=BACKGROUND_COLOR, padx=10, pady=10)
 
 # Main heading
-Label(window, text= "Option Status & Future Profitability Calculator" ,
-      font=HEADING_FONT,bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR).grid(row=0, column=0, columnspan=2,  pady=10)
+Label(window, text="Option Status & Future Profitability Calculator",
+      font=HEADING_FONT, bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR).grid(row=0, column=0, columnspan=2, pady=10)
 
 # Variables for storing inputs
 current_stock_price_var = StringVar()
@@ -120,11 +124,10 @@ profit_result_var = StringVar()
 percentage_return_var = StringVar()
 net_profit_var = StringVar()
 
-
 # ------------------------- CREATE GUI COMPONENTS, LABELS & BUTTONS --------------------------------------
 
 # Input frame
-input_frame = ttk.LabelFrame(window, text="Input Fields", padding=(10,5))
+input_frame = ttk.LabelFrame(window, text="Input Fields", padding=(10, 5))
 input_frame.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
 input_frame.grid_columnconfigure(1, weight=1)
 
@@ -138,18 +141,18 @@ create_entry(input_frame, target_price_var, 1)
 create_label(input_frame, "Option Strike Price:", 2)
 create_entry(input_frame, strike_price_var, 2)
 
-create_label(input_frame, "Option Premium (Per Share):",3)
+create_label(input_frame, "Option Premium (Per Share):", 3)
 create_entry(input_frame, premium_per_share_var, 3)
 
-# To display the profit results, percentage return, net profit and instrinsic value on GUI main window
+# To display the profit results, percentage return, net profit and intrinsic value on GUI main window
 # Results frame
-results_frame = ttk.LabelFrame(window, text="Results", padding=(10,5))
+results_frame = ttk.LabelFrame(window, text="Results", padding=(10, 5))
 results_frame.grid(row=4, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
 results_frame.grid_columnconfigure(1, weight=1)
 
-#Add results to the results frame
+# Add results to the results frame
 profit_result_label = Label(results_frame, textvariable=profit_result_var, font=RESULT_FONT)
-profit_result_label.grid(row=0, column=0, columnspan=2, pady=8, sticky=W+E)
+profit_result_label.grid(row=0, column=0, columnspan=2, pady=8, sticky=W + E)
 profit_result_label.config(bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR)
 
 percentage_return_label = Label(results_frame, textvariable=percentage_return_var, font=RESULT_FONT)
@@ -157,11 +160,11 @@ percentage_return_label.grid(row=1, column=0, columnspan=2, pady=8, sticky=W + E
 percentage_return_label.config(bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR)
 
 option_status_label = Label(results_frame, textvariable=option_status_var, font=RESULT_FONT)
-option_status_label.grid(row=2, column=0, columnspan = 2, pady=8, sticky=W + E)
+option_status_label.grid(row=2, column=0, columnspan=2, pady=8, sticky=W + E)
 option_status_label.config(bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR)
 
 intrinsic_value_label = Label(results_frame, textvariable=intrinsic_value_var, font=RESULT_FONT)
-intrinsic_value_label.grid(row=3, column=0, columnspan=2,pady=8, sticky=W + E)
+intrinsic_value_label.grid(row=3, column=0, columnspan=2, pady=8, sticky=W + E)
 intrinsic_value_label.config(bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR)
 
 net_profit_label = Label(results_frame, textvariable=net_profit_var, font=RESULT_FONT)
@@ -177,12 +180,11 @@ info_100shares_label.config(bg=BACKGROUND_COLOR, fg=FOREGROUND_COLOR)
 
 # Button used to trigger calculation
 button = ttk.Button(window, text="Calculate", command=calculate_option_details, style='TButton')
-button.grid(row=3, column=0, pady=8,padx=(0,5), sticky=E)
+button.grid(row=3, column=0, pady=8, padx=(0, 5), sticky=E)
 
 # Button used to reset all entry fields
 reset_button = ttk.Button(window, text="Reset", command=reset_fields, style='TButton')
-reset_button.grid(row=3, column=1, pady=8, padx=(5,0), sticky=W)
-
+reset_button.grid(row=3, column=1, pady=8, padx=(5, 0), sticky=W)
 
 # Button used to close / terminate app
 exit_button = ttk.Button(window, text="Exit", command=close_app, style='Exit.TButton')
@@ -199,7 +201,8 @@ style.map('TButton',
           background=[('active', BUTTON_HOVER_COLOR)],
           foreground=[('active', FOREGROUND_COLOR)])
 # Exit Button styling
-style.configure('Exit.TButton', background=EXIT_BUTTON_COLOR, foreground=FOREGROUND_COLOR, bordercolor=EXIT_BUTTON_COLOR)
+style.configure('Exit.TButton', background=EXIT_BUTTON_COLOR, foreground=FOREGROUND_COLOR,
+                bordercolor=EXIT_BUTTON_COLOR)
 style.map('Exit.TButton',
           background=[('active', EXIT_BUTTON_HOVER_COLOR)],
           foreground=[('active', FOREGROUND_COLOR)])
